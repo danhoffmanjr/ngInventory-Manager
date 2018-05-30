b9b3fd1888f1430f75a96073af009ebdad22b36c
Dan Hoffman <dghoffman@gmail.com>
2018-05-29 22:13:34 -0400

    updated layout for Product Detail component and setup route that passes the id property to the detail view from the list view. implmented a back button and onBack methed to return the user to the lsit.

--- Inventory-Manager/src/app/products/product-detail.component.ts
+++ Inventory-Manager/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,12 +8,28 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router) { }
 
   title: string = "Product Detail";
   product: IProduct;
 
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
+
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.title += `: ${id}`;
+    this.product = {
+      "id": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "rating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
   }
 
 }
